95a0203925dd9aa80de27250114b2a8241972a3d
sissidong <dongxiaofu1130@gmail.com>
2017-11-18 20:42:27 -0600

    add exp and norm

--- R/oneDsampling.R
+++ R/oneDsampling.R
@@ -12,15 +12,15 @@ ImportS: ggplot2
 #' @param ub upper bound of support of f
 
 #' @return A vector containing samples from pdf (including NA)
-#'         ggplot of original function and simulation density function
+#'         ggplot of original function(red) and simulation density function
 #'         histogram of simulation density function
 #'
 #' @export
 #'
 #' @example
 #'
-#' f<- function(x) {ifelse(0 < x & x < 1, 2*x, 0)}
-#' a<-oneDsample(f,1000, 0, 1)
+#' f<- function(x) {ifelse(-1< x & x < 0, 2*(x+1), 0)}
+#' a<-oneDsample(f,1000, -1, 0)
 #' oneDsampleplot(a)
 #'
 #' f<-function(x) {ifelse(0 <= x & x <=1, 2*(1-x), 0)}
@@ -37,11 +37,26 @@ oneDsample <- function(f, N, lb, ub) {
     stop("Error: not a pdf.The area under the function you given should be 1")
   }
   else{
-    ones <- runif(N, lb, ub)
-    maxf<-max(f(ones))+1
-    unis <- runif(N, 0, maxf)
-    data.frame(x = replicate(20000, {potentialSample <- runif(1, lb, ub);
-    ifelse(runif(1, 0, maxf) < f(potentialSample), potentialSample, NA)}))
+    if(lb!=Inf & ub!=Inf){
+      maxf<-max(f(runif(10000,lb,ub)))+1
+      data.frame(x = replicate(N, {sx <- runif(1, lb, ub);
+      ifelse(runif(1,0,maxf) < f(sx), sx, NA)}))
+      }
+    else if(lb>0 & ub==Inf){
+      maxf<-max(f(rexp(10000,rate=0.001)))+1
+      data.frame(x = replicate(N*10, {sx <- rexp(1, rate=0.001);
+      ifelse(runif(1,0,maxf+1000*dexp(sx, rate=0.001)) < f(sx), sx, NA)}))
+      }
+    else if(lb==Inf & ub!=Inf){
+      maxf<-max(f(rnorm(10000,ub,1000)))+1
+      data.frame(x = replicate(N*100, {sx <- rnorm(1, ub, 10000^2);
+      ifelse(runif(1,0,maxf+2.5*1000*dnorm(sx, ub, 1000)) < f(sx), sx, NA)}))
+      }
+    else if(lb!=Inf & ub==Inf){
+      maxf<-max(f(rnorm(10000,lb,100)))+1
+      data.frame(x = replicate(N*1000, {sx <- rnorm(1, lb, 1000);
+      ifelse(runif(1,0,maxf+2.5*1000*dnorm(sx, lb, 1000)) < f(sx), sx, NA)}))
+      }
   }
 }
 
