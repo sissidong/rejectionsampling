60ff6955cdf1f8d1ffdda33a5aa18657ee1847bf
sissidong <dongxiaofu1130@gmail.com>
2017-12-18 00:56:57 -0600

    add t distribution

--- R/oneDsampling.R
+++ R/oneDsampling.R
@@ -1,16 +1,17 @@
 #' Single variable Rejection Sampling
 #'
-#' This function 'oneDsample(f,N,lb,up)' implements single variabel rejection sampling for rvs with bounded support and which have have bounded pdf.
+#' This function 'oneDsample(f,N,lb,up,method)' implements single variabel rejection sampling for rvs with bounded support and which have have bounded pdf.
 #'
 #' Additionaly, the function 'oneDsampleplot()' and 'oneDsamplehist()' shows the plot of original function and simulation density function.
 
 ImportS: ggplot2
+Imports: metRology
 
 #' @param f       The pdf that we are sampling from
 #' @param N       The number of attempted samples.  Default value is 50000.
 #' @param lb      lower bound of support of f.      Default value is Inf.
 #' @param ub      upper bound of support of f.      Default value is Inf.
-#' @param method  There are two method you can choose, 'norm' or 'unif'.
+#' @param method  There are three method you can choose, 'norm', 't' or 'unif'.
 #'                Defalt method will be the best method we select for your pdf
 
 #' @return A vector containing samples from pdf (including NA)
@@ -22,7 +23,7 @@ ImportS: ggplot2
 #' @example
 #'
 #' f<- function(x) {ifelse(-1< x & x < 0, 2*(x+1), 0)}
-#'oneDsampleplot(oneDsample(f,20000,-1,0,'unif'))
+#'oneDsampleplot(oneDsample(f,20000,-1,0,method='unif'))
 #'
 #'f<-function(x) {ifelse(0 <= x & x <=1, 2*(1-x), 0)}
 #'oneDsampleplot(oneDsample(f,50000))
@@ -34,32 +35,39 @@ ImportS: ggplot2
 #'oneDsampleplot(oneDsample(f,50000,'norm'))
 #'
 #'f<- function(x) dnorm(x,-10,2)
-#'oneDsampleplot(oneDsample(f))
+#'oneDsampleplot(oneDsample(f,method='t'))
 #'
 #'f<- function(x) 1/(pi*(1+x^2))
 #'oneDsampleplot(oneDsample(f))
 #'
-#'
-#'
+#'f<-function(x) {dunif(x,-500,500)}
+#'oneDsampleplot(oneDsample(f,2000))
 #'
 
 unif<-function(f, N=50000, lb, ub){
-  maxf<-max(f(runif(10000,lb,ub)))+1
+  maxf<-optimize(f,c(lb,ub),maximum = TRUE)
+  maxf<-maxf$objective
   data.frame(x = replicate(N, {sx <- runif(1, lb, ub);ifelse(runif(1,0,maxf) < f(sx), sx, NA)}))
 }
 norm<-function(f, N=50000){
   x<-runif(100000,-1000,1000)
   maxf<-max(f(x))
   a=x[which( f(x) == maxf )]
-  minf<-min(f(x))
-  b=x[which( f(x) == minf )]
-  b=min((abs(b-a)))
-  sd1 = (2/maxf)*3                       # 2/maxf according to Area of triangle is 1, *3 for safty
-  #sd2 = 2/(sqrt(2*pi)*maxf)             #  for safety, make sd little large
-  sd3 = (abs(b-a))/3
-  sd=mean(sd1,sd3)
-  c= maxf/dnorm(a,a,sd)
-  data.frame(x = replicate(N, {sx <- rnorm(1,a,sd); ifelse( runif(1,0,c*dnorm(1,a,sd)) < f(sx), sx, NA)}))
+  a=mean(a)
+  sd = 2/maxf                    
+  c= 2*maxf/dnorm(a,a,sd)
+  data.frame(x = replicate(N, {sx <- rnorm(1,a,sd); ifelse( runif(1,0,c*dnorm(sx,a,sd)) < f(sx), sx, NA)}))
+}
+
+t<-function(f,N=50000){
+  x<-runif(100000,-1000,1000)
+  maxf<-max(f(x))
+  a=x[which( f(x) == maxf )]
+  a=mean(a)
+  sd=2/maxf
+  c=2*maxf/dt.scaled(a,df=1,mean=a,sd)
+  data.frame(x = replicate(N, {sx <- rt.scaled(1,1,mean=a,sd); 
+  ifelse( runif(1,0,c*dt.scaled(sx,df=1,mean=a,sd)) < f(sx), sx, NA)}))
 }
 
 oneDsample <- function(f, N=50000, lb=Inf, ub=Inf,method='best') {
@@ -71,12 +79,13 @@ oneDsample <- function(f, N=50000, lb=Inf, ub=Inf,method='best') {
       if(lb!=Inf & ub!=Inf){unif(f,N,lb,ub)}
       else{norm(f,N)}
     }
+    else if(method=='t'){t(f,N)}
     else if(method=='norm'){norm(f,N)}
     else if(method=='unif'){
       if(lb!=Inf & ub!=Inf){unif(f,N,lb,ub)}
       else{stop("Error: if your pdf without bound, you cannot use uniform method.")}
     }
-    else{stop("Error: there are two method you can choose, 'norm' or 'unif'.") }
+    else{stop("Error: there are three method you can choose, 'norm', 't' or 'unif'.") }
   }
 }
 
@@ -86,9 +95,5 @@ oneDsampleplot<-function(oneDsample){
   ggplot(w,aes(x)) + geom_density() + stat_function(fun = f, color = "red")
 }
 
-oneDsamplehist<-function(oneDsample){
-  a<-data.frame(na.omit(oneDsample))
-  a<-unlist(a)
-  hist(a)
-}
+
 
